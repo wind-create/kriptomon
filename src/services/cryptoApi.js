7a015d4ec5cf78f6200b3b7d99283af6da54ff26
wind_create <nardowilli@gmail.com>
2021-10-20 06:40:00 +0700

    sebelum news

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '40e0d2b637mshd72d7245b4e26a0p1ab31ejsn4c6f020f57fc'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
